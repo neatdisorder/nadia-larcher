bff4f2cc09019db2c16614a3e78e864e89275b8b
Nacho <nacho@Nachos-MacBook-Pro.local>
2025-02-07 18:16:08 -0300

    Planteo básico de página de videos con embebido de Youtube

--- src/components/Cards/VerticalCard/VerticalCard.js
+++ src/components/Cards/VerticalCard/VerticalCard.js
@@ -1,5 +1,41 @@
+"use client";
+
 import styles from "./VerticalCard.module.scss";
+import { useState } from "react";
 
 export default function VerticalCard({ content }) {
-  return <h1 className={styles.headline}>VerticalCard</h1>;
+  const [isEmbedOpen, setIsEmbedOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        className={styles.verticalCardContainer}
+        onClick={() => setIsEmbedOpen(true)}
+      >
+        {content.image && (
+          <img src={`https://nadialarcher.com/${content.image}`} />
+        )}
+        {content.title && <p className={styles.cardTitle}>{content.title}</p>}
+        {content.description && (
+          <p>
+            {content.subtitle && (
+              <span className={styles.boldSubtitle}>{content.subtitle} · </span>
+            )}
+            {content.description}
+          </p>
+        )}
+      </div>
+      {content.embed && isEmbedOpen && (
+        <div
+          className={styles.embedContainer}
+          onClick={() => setIsEmbedOpen(false)}
+        >
+          <div
+            className={styles.videoEmbed}
+            dangerouslySetInnerHTML={{ __html: content.embed }}
+          />
+        </div>
+      )}
+    </>
+  );
 }
