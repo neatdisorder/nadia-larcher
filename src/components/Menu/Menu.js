b1161a620b91fa9ffb45d740d10fffc0c912786c
Nacho <nacho@Nachos-MacBook-Pro.local>
2025-02-10 15:29:08 -0300

    Agregado de overlay en menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,55 +7,63 @@ export default function Menu({ content }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div
-      className={`${styles.menuContainer} ${!isMenuOpen ? styles.closed : ""}`}
-    >
-      <nav>
-        <div
-          className={styles.iconContainer}
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          <img
-            src={
-              isMenuOpen
-                ? "/assets/icons/mobile-menu-close-icon.svg"
-                : "/assets/icons/mobile-menu-icon.svg"
-            }
-          />
-        </div>
-        <div className={styles.internalLinks}>
-          <Link className={styles.titleLink} href="/es">
-            NADIA LARCHER
-          </Link>
-          <p className={styles.menuSeparator}>•</p>
-          <Link className={styles.menuLink} href="/es/musica">
-            MÚSICA
-          </Link>
-          <p className={styles.menuSeparator}>•</p>
-          <Link className={styles.menuLink} href="/es/videos">
-            VIDEOS
-          </Link>
-          <p className={styles.menuSeparator}>•</p>
-          <Link className={styles.menuLink} href="/">
-            CONTACTO
-          </Link>
-          <p className={styles.menuSeparator}>•</p>
-          <Link className={styles.menuLink} href="/">
-            ENGLISH
-          </Link>
-        </div>
-        <div className={styles.socialMediaContainer}>
-          <Link href="https://www.youtube.com/@nadia.larcher">
-            <img src="/assets/icons/youtube-icon.svg" alt="Youtube Icon" />
-          </Link>
-          <Link href="https://www.instagram.com/nadia_larcher/">
-            <img src="/assets/icons/instagram-icon.svg" alt="Instagram Icon" />
-          </Link>
-          <Link href="https://open.spotify.com/artist/7qCEjoCmUjJ0F8ACB0WUX5?si=C0xiV61TSemzzp6crbM8Bw">
-            <img src="/assets/icons/spotify-icon.svg" alt="Spotify Icon" />
-          </Link>
-        </div>
-      </nav>
-    </div>
+    <>
+      <div
+        className={`${styles.menuContainer} ${
+          !isMenuOpen ? styles.closed : ""
+        }`}
+      >
+        <nav>
+          <div
+            className={styles.iconContainer}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <img
+              src={
+                isMenuOpen
+                  ? "/assets/icons/mobile-menu-close-icon.svg"
+                  : "/assets/icons/mobile-menu-icon.svg"
+              }
+            />
+          </div>
+          <div className={styles.internalLinks}>
+            <Link className={styles.titleLink} href="/es">
+              NADIA LARCHER
+            </Link>
+            <p className={styles.menuSeparator}>•</p>
+            <Link className={styles.menuLink} href="/es/musica">
+              MÚSICA
+            </Link>
+            <p className={styles.menuSeparator}>•</p>
+            <Link className={styles.menuLink} href="/es/videos">
+              VIDEOS
+            </Link>
+            <p className={styles.menuSeparator}>•</p>
+            <Link className={styles.menuLink} href="/">
+              CONTACTO
+            </Link>
+            <p className={styles.menuSeparator}>•</p>
+            <Link className={styles.menuLink} href="/">
+              ENGLISH
+            </Link>
+          </div>
+          <div className={styles.socialMediaContainer}>
+            <Link href="https://www.youtube.com/@nadia.larcher">
+              <img src="/assets/icons/youtube-icon.svg" alt="Youtube Icon" />
+            </Link>
+            <Link href="https://www.instagram.com/nadia_larcher/">
+              <img
+                src="/assets/icons/instagram-icon.svg"
+                alt="Instagram Icon"
+              />
+            </Link>
+            <Link href="https://open.spotify.com/artist/7qCEjoCmUjJ0F8ACB0WUX5?si=C0xiV61TSemzzp6crbM8Bw">
+              <img src="/assets/icons/spotify-icon.svg" alt="Spotify Icon" />
+            </Link>
+          </div>
+        </nav>
+      </div>
+      {isMenuOpen && <div className={styles.menuOverlay}></div>}
+    </>
   );
 }
